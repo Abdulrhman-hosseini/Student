8169a216d081cb79d26fe967667ba35efe461946
Abdulrhman Hosseini <abdohosseini32@gmail.com>
2025-07-15 21:34:47 +0300

    Add OOP

---
+++ OOP/Mec.java
@@ -0,0 +1,10 @@
+package OOP;
+
+public class Mec {
+    public static void main(String[] args) {
+        Student1 s1 = new Student1();
+        s1.displayInfo();
+        s1.study();
+
+    }
+}
---
+++ OOP/Student1.java
@@ -0,0 +1,20 @@
+package OOP;
+
+public class Student1 {
+
+    public String name;
+    public int age;
+    public double gpa;
+
+
+
+    public void displayInfo(){
+        System.out.println("name " + name);
+        System.out.println("age : "+age);
+        System.out.println("gpa : " +gpa);
+    }
+    public void study(){
+        System.out.println(name);
+
+    }
+}
